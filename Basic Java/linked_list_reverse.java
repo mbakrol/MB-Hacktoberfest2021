1dd2ead781d47ae1bb578735053f17ee53c78e8b
vincyantony <91145441+vincyantony@users.noreply.github.com>
2021-09-30 22:47:17 +0530

    Hacktoberfest 2021

---
+++ Basic Java/linked_list_reverse.java
@@ -0,0 +1,62 @@
+class recursion {
+	static Node head; 
+	
+	static class Node {
+		int data;
+		Node next;
+		Node(int d)
+		{
+			data = d;
+			next = null;
+		}
+	}
+
+	static Node reverse(Node head)
+	{
+		if (head == null || head.next == null)
+			return head;
+
+		Node rest = reverse(head.next);
+		head.next.next = head;
+
+		head.next = null;
+
+		return rest;
+	}
+
+	static void print()
+	{
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+
+	static void push(int data)
+	{
+		Node temp = new Node(data);
+		temp.next = head;
+		head = temp;
+	}
+
+
+public static void main(String args[])
+{
+	
+	push(20);
+	push(4);
+	push(15);
+	push(85);
+
+	System.out.println("Given linked list");
+	print();
+
+	head = reverse(head);
+
+	System.out.println("Reversed Linked list");
+	print();
+}
+}
+
