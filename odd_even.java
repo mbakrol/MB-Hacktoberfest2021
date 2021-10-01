8c36aea3413259ca4d60cc954e24af5efde6e6b7
yogyamodi <44174310+yogyamodi@users.noreply.github.com>
2021-10-01 22:25:55 +0530

    Create odd_even.java
    Working solution for the odd even problem as described on https://practice.geeksforgeeks.org/problems/even-odd/1#
    Please let me know if any changes are required.
    Thanks.

---
+++ odd_even.java
@@ -0,0 +1,13 @@
+class Solution{
+    public void evenOdd(int a, int b){
+        if(a%2==0){
+        System.out.println(a);
+        System.out.println(b);
+        }
+        else{
+        System.out.println(b);
+        System.out.println(a);
+        }
+        
+    }
+}
