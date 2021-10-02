36a660d2c8d819e5c2342278170cdeaca5fba685
Kunal Srivastav <kunalsrivastavgs2001@gmail.com>
2021-10-02 12:16:17 +0530

    Create RangeOfPrimitiveDataTypes.java

---
+++ Basic Java/RangeOfPrimitiveDataTypes.java
@@ -0,0 +1,31 @@
+public class RangeOfPrimitiveDataTypes {
+    public static void main(String[] args) {
+        // byte range :
+        System.out.println("Range of byte is '" + Byte.MIN_VALUE + "' to '" + Byte.MAX_VALUE + "'.");
+
+        // short range :
+        System.out.println("Range of short is '" + Short.MIN_VALUE + "' to '" + Short.MAX_VALUE + "'.");
+
+        // int range :
+        System.out.println("Range of int is '" + Integer.MIN_VALUE + "' to '" + Integer.MAX_VALUE + "'.");
+
+        // long range :
+        System.out.println("Range of long is '" + Long.MIN_VALUE + "' to '" + Long.MAX_VALUE + "'.");
+
+        // char range :
+        System.out.println("Range of char is '" + Character.MIN_VALUE + "' to '" + Character.MAX_VALUE + "'.");
+
+        // ASCII char range :
+        System.out.println("ASCII Range of char is '" + (int)Character.MIN_VALUE +
+                "' to '" + (int)Character.MAX_VALUE + "'.");
+
+        // boolean range :
+        System.out.println("Range of boolean is '" + Boolean.TRUE + "' & '" + Boolean.FALSE + "'.");
+        
+        // float range :
+        System.out.println("Range of float is '" + Float.MIN_VALUE + "' to '" + Float.MAX_VALUE + "'.");
+
+        // double range :
+        System.out.println("Range of double is '" + Double.MIN_VALUE + "' to '" + Double.MAX_VALUE + "'.");
+    }
+}
